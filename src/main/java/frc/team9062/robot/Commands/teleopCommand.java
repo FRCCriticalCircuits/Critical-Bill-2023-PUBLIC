121752857d7260ad1903499d2099822d346281e4
Williams <williams.edi@gmail.com>
2023-10-03 20:02:41 -0400

    Added Drive and Arm Control

--- src/main/java/frc/team9062/robot/Commands/teleopCommand.java
+++ src/main/java/frc/team9062/robot/Commands/teleopCommand.java
@@ -1,78 +1,42 @@
 package frc.team9062.robot.Commands;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.team9062.robot.Constants.PhysicalConstants;
+import frc.team9062.robot.Constants;
+import frc.team9062.robot.Subsystems.ArmSubsystem;
 import frc.team9062.robot.Subsystems.DriveSubsystem;
+import frc.team9062.robot.Utils.IO;
+import frc.team9062.robot.Utils.SwerveDriveController;
 
 public class teleopCommand extends CommandBase{
-    private final DriveSubsystem drive_subsystem;
-    private double x,y,turn;
-    private double gain = 1;
-    private boolean field_oriented;
+    private DriveSubsystem driveSubsystem;
+    private ArmSubsystem armSubsystem;
+    private SwerveDriveController driveController;
+    private IO io;
 
-    public teleopCommand(DriveSubsystem drive_subsystem , boolean field_oriented){
-        this.drive_subsystem = drive_subsystem;
-        this.field_oriented = field_oriented;
-        addRequirements(drive_subsystem);
-    }
+    public teleopCommand(){
+      driveSubsystem = DriveSubsystem.getInstance();
+      driveController = new SwerveDriveController();
+      armSubsystem = ArmSubsystem.getInstance();
+      io = IO.getInstance();
 
-    public teleopCommand(DriveSubsystem drive_subsystem ,boolean field_oriented , double gain){
-        this.drive_subsystem = drive_subsystem;
-        this.gain = gain;
-        this.field_oriented = field_oriented;
-        addRequirements(drive_subsystem);
-    }
-
-    /**
-     * 
-     * @param input: read from controller
-     * @return 0-> if input < deadzone(0.1)
-     */
-    public double deadzone_calc(double input){
-        if(Math.abs(input) < 0.1){
-            return 0;
-        }
-        else{
-            return input;
-        }
+      addRequirements(driveSubsystem);
     }
 
     @Override
-    public void initialize() {
-        drive_subsystem.setBrakeMode(true);
-    }
+    public void initialize() {}
   
     @Override
     public void execute() {
-        x = drive_subsystem.driverJoystick.getLeftY() * gain;
-        y = drive_subsystem.driverJoystick.getLeftX() * gain;
-        turn = -drive_subsystem.driverJoystick.getRightX() * gain;
-        
-        x = deadzone_calc(x);
-        y = deadzone_calc(y);
-        turn = deadzone_calc(turn);
+      driveController.drive(
+        io.getDriverLeftY() * Constants.PhysicalConstants.MAX_TRANSLATION_SPEED_METERS,
+        io.getDriverLeftX() * Constants.PhysicalConstants.MAX_TRANSLATION_SPEED_METERS, 
+        io.getDriverRightX() * Constants.PhysicalConstants.MAX_ANGULAR_SPEED_METERS, 
+        true
+      );
 
-        final double x_speed = x * PhysicalConstants.MAX_METERS_PER_SECOND;
-        final double y_speed = y * PhysicalConstants.MAX_METERS_PER_SECOND;
-        final double turn_speed = turn * PhysicalConstants.MAX_METERS_PER_SECOND;
-
-        SwerveModuleState[] moduleStates = PhysicalConstants.KINEMATIS.toSwerveModuleStates(
-            !field_oriented 
-            ? new ChassisSpeeds(x_speed, y_speed, -turn_speed) : 
-            ChassisSpeeds.fromFieldRelativeSpeeds(x_speed, y_speed, -turn_speed, drive_subsystem.getRotation2d()) 
-            /*
-                ChassisSpeeds chassis_speeds;
-                if (!field_oriented) {
-                chassis_speeds = new ChassisSpeeds(x_speed, y_speed, turn_speed);
-                } else {
-                chassis_speeds = ChassisSpeeds.fromFieldRelativeSpeeds(x_speed, y_speed, -turn_speed, drive_subsystem.getRotation2d());
-                }
-            */
-        );
-        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, PhysicalConstants.MAX_METERS_PER_SECOND);
-        drive_subsystem.setStates(moduleStates);
+      if(Math.abs(io.getOperatorLeftY()) > 0) {
+        armSubsystem.setArm(io.getOperatorLeftY() * 0.5);
+      }
     }
   
     @Override
