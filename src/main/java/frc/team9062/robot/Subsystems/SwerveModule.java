9bb1de9982fd70bd705416711d0ff2c47a51845d
Williams <williams.edi@gmail.com>
2023-10-03 20:03:44 -0400

    Fixed Issues and Improved Subsystem

--- src/main/java/frc/team9062/robot/Subsystems/SwerveModule.java
+++ src/main/java/frc/team9062/robot/Subsystems/SwerveModule.java
@@ -9,79 +9,88 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team9062.robot.Constants;
 
 public class SwerveModule extends SubsystemBase{
-    private CANSparkMax forward;
+    private CANSparkMax drive;
     private CANSparkMax turn;
     private WPI_CANCoder canCoder;
-    private SparkMaxPIDController forwardPID, turnPID;
-    private RelativeEncoder forwardEncoder, turnEncoder;
-    private int driveMotor_ID, turn_ID, cancoder_ID;
-    private double gain,angleOffset;
-    private boolean EncoderReversed;
-    private boolean driveMotorReversed;
-    private boolean field_oriented;
-    private PIDController controller = new PIDController(0.003455, 0.000009, 0);
+    private SparkMaxPIDController drivePID, turnPID;
+    private RelativeEncoder driveEncoder, turnEncoder;
+    //private boolean field_oriented;
+    //private PIDController controller = new PIDController(0.003455, 0.000009, 0);
 
-    public SwerveModule(int driveMotor_ID, int turn_ID, int cancoder_ID, double angleOffset, boolean EncoderReversed, boolean field_oriented,boolean driveMotorReversed) {
-        this.driveMotor_ID = driveMotor_ID;
-        this.turn_ID = turn_ID;
-        this.cancoder_ID = cancoder_ID;
-        this.angleOffset = angleOffset;
-        this.EncoderReversed = EncoderReversed;
-        this.field_oriented = field_oriented;
-        this.driveMotorReversed = driveMotorReversed;
-
-        forward = new CANSparkMax(driveMotor_ID, MotorType.kBrushless);
+    public SwerveModule(int driveMotor_ID, int turn_ID, int cancoder_ID, double angleOffset, boolean EncoderReversed, boolean driveMotorReversed) {
+        drive = new CANSparkMax(driveMotor_ID, MotorType.kBrushless);
         turn = new CANSparkMax(turn_ID, MotorType.kBrushless);
         canCoder = new WPI_CANCoder(cancoder_ID);
 
-        forwardPID = forward.getPIDController();
+        drive.restoreFactoryDefaults();
+        turn.restoreFactoryDefaults();
+
+        drivePID = drive.getPIDController();
         turnPID = turn.getPIDController();
 
-        forwardEncoder = forward.getEncoder();
+        driveEncoder = drive.getEncoder();
         turnEncoder = turn.getEncoder();
         
-        forwardPID.setFeedbackDevice(forwardEncoder);
-        turnPID.setFeedbackDevice(turnEncoder);
-        
-        forwardEncoder.setPosition(0.0);
-        turnEncoder.setPosition(canCoder.getAbsolutePosition());
-
-        forward.setInverted(driveMotorReversed);
-        turn.setInverted(true);
-        forwardPID.setSmartMotionMaxVelocity(Constants.PhysicalConstants.MAX_VELOCITY_RPM, 0);
-        forward.setSmartCurrentLimit(40);
-        turn.setSmartCurrentLimit(40);
-        canCoder.configSensorDirection(EncoderReversed);
-        canCoder.configMagnetOffset(angleOffset);
-        canCoder.setPosition(getAbsoluteAngle());
-        turnEncoder.setPositionConversionFactor(((1 / Constants.PhysicalConstants.ROTATION_GEAR_RATIO) * 360));
-        forwardEncoder.setPositionConversionFactor(Units.inchesToMeters((1 / Constants.PhysicalConstants.DRIVE_GEAR_RATIO * Math.PI * 4)));
-        forwardEncoder.setVelocityConversionFactor(Units.inchesToMeters(((1 / Constants.PhysicalConstants.DRIVE_GEAR_RATIO * Math.PI * 4))) / 60);
         canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);
+        canCoder.configMagnetOffset(angleOffset);
+
+        drive.setInverted(driveMotorReversed);
+        turn.setInverted(true);
+
+        //drivePID.setSmartMotionMaxVelocity(Constants.PhysicalConstants.MAX_VELOCITY_RPM, 0);
+        drive.setSmartCurrentLimit(40);
+        turn.setSmartCurrentLimit(40);
+
+        driveEncoder.setPositionConversionFactor(Units.inchesToMeters(Constants.PhysicalConstants.DRIVE_GEAR_RATIO * Math.PI * 4));
+        driveEncoder.setVelocityConversionFactor(Units.inchesToMeters(Constants.PhysicalConstants.DRIVE_GEAR_RATIO * Math.PI * 4) / 60);
+        turnEncoder.setPositionConversionFactor((1 / Constants.PhysicalConstants.TURN_GEAR_RATIO) * Math.PI * 2);
+        turnEncoder.setVelocityConversionFactor(((1 / Constants.PhysicalConstants.TURN_GEAR_RATIO) * Math.PI * 2) / 60);
+    
+        canCoder.configSensorDirection(EncoderReversed);
+        canCoder.setPositionToAbsolute();
+
+        driveEncoder.setPosition(0.0);
+        turnEncoder.setPosition(Math.toRadians(canCoder.getAbsolutePosition()));
+
+        drivePID.setP(Constants.TunedConstants.PIDF0_DRIVE_P, 0);
+        drivePID.setI(Constants.TunedConstants.PIDF0_DRIVE_I, 0);
+        drivePID.setD(Constants.TunedConstants.PIDF0_DRIVE_D, 0);
+        drivePID.setFF(Constants.TunedConstants.PIDF0_DRIVE_F, 0);
+
+        turnPID.setP(Constants.TunedConstants.PIDF0_TURN_P, 0);
+        turnPID.setI(Constants.TunedConstants.PIDF0_TURN_I, 0);
+        turnPID.setD(Constants.TunedConstants.PIDF0_TURN_D, 0);
+        turnPID.setFF(Constants.TunedConstants.PIDF0_TURN_F, 0);
+        //turnPID.setOutputRange(-Math.PI, Math.PI, 0);
+        //turnPID.setIZone(1);
+
+        drivePID.setFeedbackDevice(driveEncoder);
+        turnPID.setFeedbackDevice(turnEncoder);
+
+        turn.enableVoltageCompensation(Constants.PhysicalConstants.NOMINAL_VOLTAGE);
+        drive.enableVoltageCompensation(Constants.PhysicalConstants.NOMINAL_VOLTAGE);
 
         setBrakeMode(true);
 
-        forward.burnFlash();
+        drive.burnFlash();
         turn.burnFlash();
     }
 
-    public void setForwardPID(double p, double i, double d, double f, int slotID) {
-        forwardPID.setP(p, slotID);
-        forwardPID.setI(i, slotID);
-        forwardPID.setD(d, slotID);
-        forwardPID.setFF(f, slotID);
+    public void setdrivePID(double p, double i, double d, double f, int slotID) {
+        drivePID.setP(p, slotID);
+        drivePID.setI(i, slotID);
+        drivePID.setD(d, slotID);
+        drivePID.setFF(f, slotID);
 
-        forward.burnFlash();
+        drive.burnFlash();
     }    
     
     public void setTurnPID(double p, double i, double d, double f, int slotID) {
@@ -89,7 +98,7 @@ public class SwerveModule extends SubsystemBase{
         turnPID.setI(i, slotID);
         turnPID.setD(d, slotID);
         turnPID.setFF(f, slotID);
-        turnPID.setIZone(1);
+        //turnPID.setIZone(1);
         //turnPID.setOutputRange(-180, 180);
 
         turn.burnFlash();
@@ -97,62 +106,67 @@ public class SwerveModule extends SubsystemBase{
 
     public void setBrakeMode(Boolean mode){
         if(mode){
-            forward.setIdleMode(IdleMode.kBrake);
+            drive.setIdleMode(IdleMode.kBrake);
             turn.setIdleMode(IdleMode.kBrake);
         }else{
-            forward.setIdleMode(IdleMode.kCoast);
+            drive.setIdleMode(IdleMode.kCoast);
             turn.setIdleMode(IdleMode.kCoast);        
         }
     }
 
     public void resetEncoder(){
-        forwardEncoder.setPosition(0);
+        driveEncoder.setPosition(0);
         canCoder.setPosition(getAbsoluteAngle());
     }
 
-    public double closestAngle(double angle) {
-        double dir = (angle % 360) - (getAngleCancoder() % 360);
-
-        if(Math.abs(dir) > 180){
-            dir = -(Math.signum(dir) * 360) + dir;
+    public void checkEncoder() {
+        if(getAngle() > Math.PI) {
+            turnEncoder.setPosition(getAngle() - (2 * Math.PI));
+        } else if(getAngle() < -Math.PI) {
+            turnEncoder.setPosition(getAngle() + (2 * Math.PI));
         }
+    }
+
+    public double closestAngle(double angle) {
+        double dir = (angle % (2 * Math.PI)) - (getAngle() % (2 * Math.PI));
+
+        if(Math.abs(dir) > Math.PI){
+            dir = -(Math.signum(dir) * (2 * Math.PI)) + dir;
+        }
+
         return dir;
     }
 
     public void setAngle(double angle) {
         double setpointAngle = closestAngle(angle);
-        double setpointAngleInvert = closestAngle(angle + 180);
+        double setpointAngleInvert = closestAngle(angle + Math.PI);
 
         if(Math.abs(setpointAngle) <= Math.abs(setpointAngleInvert)){
-            gain = 1;
-            if(field_oriented){
-                turnPID.setReference(getAngleCancoder() + setpointAngle, ControlType.kPosition);
-            }else{
-                turnPID.setReference(controller.calculate(getAngleCancoder(), getAngleCancoder() + setpointAngle), ControlType.kDutyCycle);
-            }
+            turnPID.setReference(getAngle() + setpointAngle, ControlType.kPosition);
         }else{
-            gain = -1;
-            if(field_oriented){
-                turnPID.setReference(getAngleCancoder() + setpointAngleInvert, ControlType.kPosition);
-            }else{
-                turnPID.setReference(controller.calculate(getAngleCancoder(), getAngleCancoder() + setpointAngleInvert), ControlType.kDutyCycle);
-            }
+            turnPID.setReference(getAngle() + setpointAngleInvert, ControlType.kPosition);
         }
-        
     }
-    public void setState(SwerveModuleState state, boolean isOpenLoop){
+
+    public void setState(SwerveModuleState state){
         SwerveModuleState desiredState = SwerveModuleState.optimize(state, getRotation());
         double velocity = desiredState.speedMetersPerSecond;
-        double moduleangle = desiredState.angle.getDegrees();
-        forwardPID.setReference(velocity, ControlType.kVelocity);
-        turnPID.setReference(controller.calculate(getAngleCancoder(), getAngleCancoder() + closestAngle(moduleangle)), ControlType.kDutyCycle);
+        double moduleangle = desiredState.angle.getRadians();
+        //drivePID.setReference(velocity, ControlType.kVelocity);
+        //drivePID.setReference(
+        //    (velocity / Constants.PhysicalConstants.MAX_WHEEL_SPEED_METERS), 
+        //    ControlType.kDutyCycle
+        //);
+        drive.set(velocity / Constants.PhysicalConstants.MAX_WHEEL_SPEED_METERS);
+        //turnPID.setReference(moduleangle, ControlType.kPosition);
+        setAngle(moduleangle);
 
-        SmartDashboard.getNumber("TARGET ANGLE " + cancoder_ID / 3, moduleangle);
-        SmartDashboard.getNumber("ANGLE " + cancoder_ID / 3, moduleangle);
+        //SmartDashboard.getNumber("TARGET ANGLE " + canCoder.getDeviceID() / 3, moduleangle);
+        //SmartDashboard.getNumber("ANGLE " + canCoder.getDeviceID() / 3, moduleangle);
     }
 
-    public double getSpeed(){
-        return forwardEncoder.getVelocity();
+    public double getVelocity(){
+        return driveEncoder.getVelocity();
     }
 
     public double getAngle() {
@@ -168,26 +182,30 @@ public class SwerveModule extends SubsystemBase{
     }
 
     public Rotation2d getRotation() {
-        return Rotation2d.fromDegrees(getAngleCancoder());
+        return Rotation2d.fromRadians(getAngle());
     }
 
     public double getDistance() {
-        return forwardEncoder.getPosition();
+        return driveEncoder.getPosition();
     }
 
     public SwerveModuleState getModuleState() {
         return new SwerveModuleState(
-           getSpeed(), getRotation()
+           getVelocity(), 
+           getRotation()
         );
     }
 
     public SwerveModulePosition getModulePosition() {
-        return new SwerveModulePosition(
+        return new SwerveModulePosition( 
             getDistance(), 
             getRotation()
         );
     }
+    
     @Override
     public void periodic(){
+        checkEncoder();
+        
     }
 }
