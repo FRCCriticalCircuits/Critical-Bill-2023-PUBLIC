cf705a8522bb78fffa20d86b5a92d4c36a54abae
Williams <williams.edi@gmail.com>
2023-10-03 20:03:18 -0400

    Fixed Errors and Cleaned Up Code

--- src/main/java/frc/team9062/robot/Subsystems/DriveSubsystem.java
+++ src/main/java/frc/team9062/robot/Subsystems/DriveSubsystem.java
@@ -7,19 +7,28 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team9062.robot.Constants;
 import frc.team9062.robot.Constants.IDs;
-import frc.team9062.robot.Constants.TunedConstants;
 import frc.team9062.robot.Constants.PhysicalConstants;
 
 public class DriveSubsystem extends SubsystemBase{
+    private static DriveSubsystem instance;
+
+    public static DriveSubsystem getInstance() {
+      if(instance == null) {
+        instance = new DriveSubsystem();
+      } 
+  
+      return instance;
+    }
+
     public SwerveModule frontLeft = new SwerveModule(
       IDs.FRONT_LEFT_FORWARD_ID,
       IDs.FRONT_LEFT_ROTATION_ID,
       IDs.FRONT_LEFT_CANCODER_ID,
-      56.6 - 114,
-      false,
+      157.1,
       false,
       false
     );
@@ -28,18 +37,16 @@ public class DriveSubsystem extends SubsystemBase{
         IDs.FRONT_RIGHT_FORWARD_ID, 
         IDs.FRONT_RIGHT_ROTATION_ID,
         IDs.FRONT_RIGHT_CANCODER_ID,
-        -64.863 + 129,
+        63,
         false,
-        false,
-        false
+        true
     );
 
     public SwerveModule rearLeft = new SwerveModule(
         IDs.REAR_LEFT_FORWARD_ID, 
         IDs.REAR_LEFT_ROTATION_ID,
         IDs.REAR_LEFT_CANCODER_ID,
-        -65 + 130,
-        false,
+        -66.2,
         false,
         false
     );
@@ -48,80 +55,19 @@ public class DriveSubsystem extends SubsystemBase{
         IDs.REAR_RIGHT_FORWARD_ID, 
         IDs.REAR_RIGHT_ROTATION_ID,
         IDs.REAR_RIGHT_CANCODER_ID,
-        -50 - 104,
+        121.9,
         false,
-        false,
-        false
+        true
     );
 
-    public XboxController driverJoystick = new XboxController(0);
-
-    private AHRS gyro = new AHRS(SerialPort.Port.kUSB);
-
-    private double GYRO_offset = 0;
-
+  private AHRS gyro = new AHRS(SerialPort.Port.kUSB);
 
   public DriveSubsystem() {
-    frontLeft.setForwardPID(
-        TunedConstants.PIDF0_DRIVE_P, 
-        TunedConstants.PIDF0_DRIVE_I, 
-        TunedConstants.PIDF0_DRIVE_D, 
-        TunedConstants.PIDF0_DRIVE_F, 0
-    );
-
-    frontLeft.setTurnPID(
-        TunedConstants.PIDF0_TURN_P, 
-        TunedConstants.PIDF0_TURN_I, 
-        TunedConstants.PIDF0_TURN_D, 
-        TunedConstants.PIDF0_TURN_F, 0
-    );
-
-    frontRight.setForwardPID(
-        TunedConstants.PIDF0_DRIVE_P, 
-        TunedConstants.PIDF0_DRIVE_I, 
-        TunedConstants.PIDF0_DRIVE_D, 
-        TunedConstants.PIDF0_DRIVE_F, 0
-    );
-
-    frontRight.setTurnPID(
-        TunedConstants.PIDF0_TURN_P, 
-        TunedConstants.PIDF0_TURN_I, 
-        TunedConstants.PIDF0_TURN_D, 
-        TunedConstants.PIDF0_TURN_F, 0
-    );
-
-    rearLeft.setForwardPID(
-        TunedConstants.PIDF0_DRIVE_P, 
-        TunedConstants.PIDF0_DRIVE_I, 
-        TunedConstants.PIDF0_DRIVE_D, 
-        TunedConstants.PIDF0_DRIVE_F, 0
-    );
-
-    rearLeft.setTurnPID(
-        TunedConstants.PIDF0_TURN_P, 
-        TunedConstants.PIDF0_TURN_I, 
-        TunedConstants.PIDF0_TURN_D, 
-        TunedConstants.PIDF0_TURN_F, 0
-    );
-
-    rearRight.setForwardPID(
-        TunedConstants.PIDF0_DRIVE_P, 
-        TunedConstants.PIDF0_DRIVE_I, 
-        TunedConstants.PIDF0_DRIVE_D, 
-        TunedConstants.PIDF0_DRIVE_F, 0
-    );
-
-    rearRight.setTurnPID(
-        TunedConstants.PIDF0_TURN_P, 
-        TunedConstants.PIDF0_TURN_I, 
-        TunedConstants.PIDF0_TURN_D, 
-        TunedConstants.PIDF0_TURN_F, 0
-    );
-
     new Thread(() -> {
         try {
           Thread.sleep(2000);
           resetHeading();
+          Thread.currentThread().interrupt();
         } catch (Exception e) {
         }
       }
@@ -143,21 +89,14 @@ public class DriveSubsystem extends SubsystemBase{
     frontRight.resetEncoder();
     rearLeft.resetEncoder();
     rearRight.resetEncoder();
-    resetHeading();
   }
 
   public void resetHeading() {
     gyro.zeroYaw();
   }
 
-  /**
-   * RETURNS THE HEADING OF THE ROBOT
-   * IN ROTATION2D
-   *
-   * @return heading
-  */
   public double getHeading() {
-    return gyro.getYaw() * -1 - GYRO_offset;
+    return gyro.getYaw() * (Constants.PhysicalConstants.isGyroReversed ? -1 : 1) - Constants.PhysicalConstants.GYRO_OFFSET;
   }
 
   public Rotation2d getRotation2d() {
@@ -178,33 +117,18 @@ public class DriveSubsystem extends SubsystemBase{
     SwerveModulePosition rearleft = rearLeft.getModulePosition();
     SwerveModulePosition rearright = rearRight.getModulePosition();
 
-    SwerveModulePosition swerveModulePosition[] = {frontleft, frontright, rearleft, rearright};
+    SwerveModulePosition swerveModulePositions[] = {frontleft, frontright, rearleft, rearright};
 
-    return swerveModulePosition;
+    return swerveModulePositions;
   }
-  /**
-   * Outputs robot information,
-   * to be used for autonomous purposes
-   * 
-   * @param states Desires states of the swerve modules
-   * 
-   * @return Makes the robot move
-   * @return Output Information aboutb the robot on shuffleboard
-   */
+
   public void OutputModuleInfo(SwerveModuleState[] states) {
-    SwerveDriveKinematics.desaturateWheelSpeeds(states, PhysicalConstants.MAX_METERS_PER_SECOND);
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, PhysicalConstants.MAX_WHEEL_SPEED_METERS);
 
-    frontLeft.setState(states[0], false);
-    frontRight.setState(states[1], false);
-    rearLeft.setState(states[2], false);
-    rearRight.setState(states[3], false);
-  }
-
-  public void setStates(SwerveModuleState[] state) {
-    frontLeft.setState(state[0], true); // 2
-    frontRight.setState(state[1], true); // 3
-    rearLeft.setState(state[2], true); // 0
-    rearRight.setState(state[3], true); // 1
+    frontLeft.setState(states[0]);
+    frontRight.setState(states[1]);
+    rearLeft.setState(states[2]);
+    rearRight.setState(states[3]);
   }
 
   public void OutputChassisSpeeds(ChassisSpeeds speeds) {
@@ -213,6 +137,11 @@ public class DriveSubsystem extends SubsystemBase{
 
   @Override
   public void periodic() {
-  }
+    SmartDashboard.putNumber("FRONT LEFT ANGLE", Math.toDegrees(frontLeft.getAngle()));
+    SmartDashboard.putNumber("FRONT RIGHT ANGLE", Math.toDegrees(frontRight.getAngle()));
+    SmartDashboard.putNumber("REAR LEFT ANGLE", Math.toDegrees(rearLeft.getAngle()));
+    SmartDashboard.putNumber("REAR RIGHT ANGLE", Math.toDegrees(rearRight.getAngle()));
 
+    SmartDashboard.putNumber("ANGLE", getHeading());
+  }
 }
