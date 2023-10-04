c85683fddcbc4386b454a7664218495f3d258ed4
Williams <williams.edi@gmail.com>
2023-10-03 20:04:53 -0400

    Added IO

--- src/main/java/frc/team9062/robot/Utils/IO.java
+++ src/main/java/frc/team9062/robot/Utils/IO.java
@@ -8,10 +8,13 @@ public class IO {
     private static IO instance;
     private XboxController driverGamepad;
     private GenericHID operatorGamepad;
+    private CriticalDeadband deadband;
 
     public IO() {
         driverGamepad = new XboxController(Constants.Controller.DRIVER_ID);
         operatorGamepad = new GenericHID(Constants.Controller.OPERATOR_ID);
+        deadband = new CriticalDeadband(0.15);
+        deadband.setMinMax(-1, 1);
     }
 
     public static IO getInstance() {
@@ -20,5 +23,37 @@ public class IO {
         }
 
         return instance;
+    }
+
+    public double getDriverLeftX() {
+        return deadband.applydeadband(() -> driverGamepad.getLeftX());
+    }
+
+    public double getDriverLeftY() {
+        return deadband.applydeadband(() -> driverGamepad.getLeftY());
+    }
+
+    public double getDriverRightX() {
+        return deadband.applydeadband(() -> driverGamepad.getRightX());
+    }
+
+    public double getDriverRightY() {
+        return deadband.applydeadband(() -> driverGamepad.getRightY());
+    }
+
+    public boolean getDriverLeftTrigger() {
+        return driverGamepad.getLeftTriggerAxis() > 0.8;
+    }
+
+    public boolean getDriverRightTrigger() {
+        return driverGamepad.getRightTriggerAxis() > 0.8;
+    }
+
+    public double getOperatorLeftX() {
+        return deadband.applydeadband(operatorGamepad.getRawAxis(0));
+    }
+
+    public double getOperatorLeftY() {
+        return deadband.applydeadband(operatorGamepad.getRawAxis(1));
     }
 }
