10fe78d457fab18dee3464cd90c09ed0087e1986
Williams <williams.edi@gmail.com>
2023-10-11 09:41:24 -0400

    Added Driver Controller Feedback

--- src/main/java/frc/team9062/robot/Commands/teleopCommand.java
+++ src/main/java/frc/team9062/robot/Commands/teleopCommand.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team9062.robot.Constants;
 import frc.team9062.robot.Subsystems.ArmSubsystem;
 import frc.team9062.robot.Subsystems.DriveSubsystem;
+import frc.team9062.robot.Utils.ControllerBinds;
 import frc.team9062.robot.Utils.IO;
 import frc.team9062.robot.Utils.SwerveDriveController;
 
@@ -11,15 +12,19 @@ public class teleopCommand extends CommandBase{
     private DriveSubsystem driveSubsystem;
     private ArmSubsystem armSubsystem;
     private SwerveDriveController driveController;
+    private ControllerBinds binds;
     private IO io;
+    private double lastVel = 0;
+    private boolean manualArmControl = true;
 
     public teleopCommand(){
       driveSubsystem = DriveSubsystem.getInstance();
       driveController = new SwerveDriveController();
       armSubsystem = ArmSubsystem.getInstance();
       io = IO.getInstance();
+      binds = ControllerBinds.getInstance();
 
-      addRequirements(driveSubsystem);
+      addRequirements(driveSubsystem, armSubsystem);
     }
 
     @Override
@@ -27,6 +32,7 @@ public class teleopCommand extends CommandBase{
   
     @Override
     public void execute() {
+      // Drive
       driveController.drive(
         io.getDriverLeftY() * Constants.PhysicalConstants.MAX_TRANSLATION_SPEED_METERS,
         io.getDriverLeftX() * Constants.PhysicalConstants.MAX_TRANSLATION_SPEED_METERS, 
@@ -34,9 +40,33 @@ public class teleopCommand extends CommandBase{
         true
       );
 
-      if(Math.abs(io.getOperatorLeftY()) > 0) {
-        armSubsystem.setArm(io.getOperatorLeftY() * 0.5);
+      if(manualArmControl) {
+        armSubsystem.setArm(-io.getOperatorLeftY());
+      } else {
+        
       }
+
+      armSubsystem.setShoulder(io.getOperatorRightY());
+
+      // Intake Binds
+      if(binds.ConeIntake()) {
+        armSubsystem.setConeIntake();
+      } else if(binds.CubeIntake()) {
+        armSubsystem.setCubeIntake();
+      } else if(binds.Outtake()) {
+        armSubsystem.setIntake(-0.5);
+      } else {
+        armSubsystem.setIntake(0);
+      }
+
+      // Use rate of change in robot velocity to set controller feedback
+      double velocity = Math.sqrt(Math.pow(driveSubsystem.getXAccelVel(), 2) + Math.pow(driveSubsystem.getYAccelVel(), 2));
+      double accel = (velocity - lastVel) / Constants.LOOP_TIME_S;
+      
+      accel = Math.abs(accel) > Constants.PhysicalConstants.MAX_WHEEL_SPEED_METERS ? 1 : accel / Constants.PhysicalConstants.MAX_WHEEL_SPEED_METERS;
+      io.setDriverRumble((accel * Math.signum(accel)) * Constants.Controller.DRIVER_RUMBLE_STRENGTH);
+
+      lastVel = velocity;
     }
   
     @Override
