78c3e92ea32551fa96acbf1514bd9f859f775486
Williams <williams.edi@gmail.com>
2023-10-11 09:38:36 -0400

    Added More Inputs and Added Controller Rumble

--- src/main/java/frc/team9062/robot/Utils/IO.java
+++ src/main/java/frc/team9062/robot/Utils/IO.java
@@ -1,18 +1,18 @@
 package frc.team9062.robot.Utils;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import frc.team9062.robot.Constants;
 
 public class IO {
     private static IO instance;
     private XboxController driverGamepad;
-    private GenericHID operatorGamepad;
+    private XboxController operatorGamepad;
     private CriticalDeadband deadband;
 
     public IO() {
         driverGamepad = new XboxController(Constants.Controller.DRIVER_ID);
-        operatorGamepad = new GenericHID(Constants.Controller.OPERATOR_ID);
+        operatorGamepad = new XboxController(Constants.Controller.OPERATOR_ID);
         deadband = new CriticalDeadband(0.15);
         deadband.setMinMax(-1, 1);
     }
@@ -49,11 +49,115 @@ public class IO {
         return driverGamepad.getRightTriggerAxis() > 0.8;
     }
 
+    public boolean getDriverLeftBumper() {
+        return driverGamepad.getLeftBumper();
+    }
+
+    public boolean getDriverRightBumper() {
+        return driverGamepad.getRightBumper();
+    }
+
+    public boolean getDriverXButton() {
+        return driverGamepad.getXButton();
+    }
+
+    public boolean getDriverYButton() {
+        return driverGamepad.getYButton();
+    }
+
+    public boolean getDriverAButton() {
+        return driverGamepad.getAButton();
+    }
+
+    public boolean getDriverBButton() {
+        return driverGamepad.getBButton();
+    }
+
+    public boolean getDriverPOVUP() {
+        return driverGamepad.getPOV() == 0;
+    }
+
+    public boolean getDriverPOVRIGHT() {
+        return driverGamepad.getPOV() == 90;
+    }
+
+    public boolean getDriverPOVDOWN() {
+        return driverGamepad.getPOV() == 180;
+    }
+
+    public boolean getDriverPOVLEFT() {
+        return driverGamepad.getPOV() == 270;
+    }
+
     public double getOperatorLeftX() {
-        return deadband.applydeadband(operatorGamepad.getRawAxis(0));
+        return deadband.applydeadband(() -> operatorGamepad.getLeftX());
     }
 
     public double getOperatorLeftY() {
-        return deadband.applydeadband(operatorGamepad.getRawAxis(1));
+        return deadband.applydeadband(() -> operatorGamepad.getLeftY());
+    }
+
+    public double getOperatorRightX() {
+        return deadband.applydeadband(() -> operatorGamepad.getRightX());
+    }
+
+    public double getOperatorRightY() {
+        return deadband.applydeadband(() -> operatorGamepad.getRightY());
+    }
+
+    public boolean getOperatorLeftTrigger() {
+        return operatorGamepad.getLeftTriggerAxis() > 0.8;
+    }
+
+    public boolean getOperatorRightTrigger() {
+        return operatorGamepad.getRightTriggerAxis() > 0.8;
+    }
+
+    public boolean getOperatorLeftBumper() {
+        return operatorGamepad.getLeftBumper();
+    }
+
+    public boolean getOperatorRightBumper() {
+        return operatorGamepad.getRightBumper();
+    }
+
+    public boolean getOperatorXButton() {
+        return operatorGamepad.getXButton();
+    }
+
+    public boolean getOperatorYButton() {
+        return operatorGamepad.getYButton();
+    }
+
+    public boolean getOperatorAButton() {
+        return operatorGamepad.getAButton();
+    }
+
+    public boolean getOperatorBButton() {
+        return operatorGamepad.getBButton();
+    }
+
+    public boolean getOperatorPOVUP() {
+        return operatorGamepad.getPOV() == 0;
+    }
+
+    public boolean getOperatorPOVRIGHT() {
+        return operatorGamepad.getPOV() == 90;
+    }
+
+    public boolean getOperatorPOVDOWN() {
+        return operatorGamepad.getPOV() == 180;
+    }
+
+    public boolean getOperatorPOVLEFT() {
+        return operatorGamepad.getPOV() == 270;
+    }
+
+    public void setDriverRumble(double value) {
+        driverGamepad.setRumble(RumbleType.kBothRumble, value);
+    }
+
+    public void setOperatorRumble(double value) {
+        driverGamepad.setRumble(RumbleType.kBothRumble, value);
     }
 }
