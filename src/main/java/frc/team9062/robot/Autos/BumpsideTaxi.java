a090e25141bad9af3e5f160a6777efceb36bae52
Williams <williams.edi@gmail.com>
2023-10-11 09:35:40 -0400

    Added Autos

---
+++ src/main/java/frc/team9062/robot/Autos/BumpsideTaxi.java
@@ -0,0 +1,7 @@
+package frc.team9062.robot.Autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class BumpsideTaxi extends SequentialCommandGroup {
+    
+}
---
+++ src/main/java/frc/team9062/robot/Autos/MiddleTaxi.java
@@ -0,0 +1,9 @@
+package frc.team9062.robot.Autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class MiddleTaxi extends SequentialCommandGroup{
+    public MiddleTaxi() {
+        
+    }
+}
