72022a1c5db3134099f95e7dd6186cf268a3059f
Williams <williams.edi@gmail.com>
2023-10-18 13:26:06 -0400

    Added More Binds

--- src/main/java/frc/team9062/robot/Utils/ControllerBinds.java
+++ src/main/java/frc/team9062/robot/Utils/ControllerBinds.java
@@ -1,8 +1,11 @@
 package frc.team9062.robot.Utils;
 
+import edu.wpi.first.math.filter.Debouncer;
+
 public class ControllerBinds {
     private static ControllerBinds instance;
     private static IO io;
+    private Debouncer debouncer = new Debouncer(0.5);
 
     public ControllerBinds() {
         io = IO.getInstance();
@@ -26,5 +29,33 @@ public class ControllerBinds {
 
     public boolean Outtake() {
         return io.getOperatorXButton();
+    }
+
+    public boolean setArmStowed() {
+        return io.getOperatorAButton();
+    }
+
+    public boolean setArmHigh() {
+        return io.getOperatorRightBumper() && io.getOperatorYButton();
+    }
+
+    public boolean setArmMid() {
+        return io.getOperatorRightBumper() && io.getOperatorXButton();
+    }
+
+    public boolean setArmLow() {
+        return io.getOperatorRightBumper() && io.getOperatorAButton();
+    }
+
+    public boolean setArmHighInverted() {
+        return io.getOperatorLeftBumper() && io.getOperatorYButton();
+    }
+
+    public boolean setArmMidInverted() {
+        return io.getOperatorLeftBumper() && io.getOperatorXButton();
+    }
+
+    public boolean resetHeading() {
+        return debouncer.calculate(io.getDriverYButton());
     }
 }
