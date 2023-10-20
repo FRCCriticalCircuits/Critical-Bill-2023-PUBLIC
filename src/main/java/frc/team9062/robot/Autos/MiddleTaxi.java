c56d1df8ee55e36376e46405b81c08ac195204fe
Williams <williams.edi@gmail.com>
2023-10-20 10:55:03 -0400

    Removed Unnecessary Files

--- src/main/java/frc/team9062/robot/Autos/MiddleTaxi.java
+++
@@ -1,9 +0,0 @@
-package frc.team9062.robot.Autos;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-public class MiddleTaxi extends SequentialCommandGroup{
-    public MiddleTaxi() {
-        
-    }
-}
